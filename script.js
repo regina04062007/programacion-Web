d3668452fe9490f5c36ceccfde230b008f063bb2
regina04062007 <rherreram2200@alumno.ipn.mx>
2024-09-24 17:56:14 -0600

    programWeb

---
+++ script.js
@@ -0,0 +1,29 @@
+//CLASE 24/09/2024
+//NOMBRE:HERRERA MACEDO REGINA ELIZABETH; GUÍZAR ORTEGA JOSÉ LUIS
+
+//variables
+/*
+las variables guardan informacion que puedes reutilizar
+*/
+//declaracion de variables
+
+let nombre='Pedro'; //usa let para declarar variables que pueden cambiar su  valor
+const PI= 3.1416;
+var edad=25;
+// las variables let despues de ser utilizadas en una funcion ya no pueden camnbiar su valor 
+
+//operadores
+let a=5;
+let b=2;
+let suma=a+b;
+let resta=a-b;
+let nultiplicacion= a*b;
+let division=a/b;
+
+
+//operadores de comparacion
+a>b;//true
+a<b;//false
+a==b;//false
+a===b;//false
+
