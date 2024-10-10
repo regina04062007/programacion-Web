942f7b51f579fd44285abd6f74475f48b76e3929
regina04062007 <rherreram2200@alumno.ipn.mx>
2024-10-10 16:43:50 -0600

    10102024 funciones

---
+++ 10102024/script.js
@@ -0,0 +1,9 @@
+//Declaramos nuestra función
+function hello (){
+    return 'hola js'
+}
+
+console.log(hello());
+
+//Tarea investigar funciones en js y sus diferentes usos en js y ejemplificar cada una de ellas
+//Funcion dentro de otra funcion, por parametros, 
