cbe0e028508ca09f388137e0d1fb64e418f5f9f8
regina04062007 <rherreram2200@alumno.ipn.mx>
2024-10-24 16:24:30 -0600

    push, arreglo, for

---
+++ Clase_24102024/script.js
@@ -0,0 +1,33 @@
+/*
+//arreglos
+let array = ["dato", 1, Boolean];
+console.log("El tamaño del array es "+ array.length);
+
+array.push("Nuevo valor agregado");
+console.log(array[3]);
+console.log("El nuevo tamaño del array es: "+array.length);
+
+*/
+//length, push, for
+
+let tabla5=[];
+
+for (i=1; i<11; i++){
+    if(tabla5.length<6){
+        tabla5.push("El resultado de 5*"+i+ "="+ i*5);
+        console.log(tabla5);
+    }
+}
+console.log("Aqui termina el arreglo");
+
+
+//Eliminar elementos de un array
+
+//let array = ["dato 1", 1, Boolean];
+
+//array.push("insert nuevo valor");
+//console.log(array[3]);
+
+//Eliminar
+//array.slice(2,2)
+//console.log(array);
